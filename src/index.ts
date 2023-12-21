7e6c60527b13204dcfc1ef3f6e17b9f47dce5a49
Jeremy <73146752+JeremyRio@users.noreply.github.com>
2023-12-21 13:15:58 +0700

    chore: fix gitignore and added package-lock
    Co-Authored-By: Dzaky Fattan <70198829+DzakyFattan@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -46,7 +46,6 @@ app.post("/login", async (req: Request, res: Response) => {
 
     if (user && bcrypt.compareSync(password, user.password)) {
       const token = generateToken(user.id);
-
       res.status(200).json({ message: "Login successful", token: token });
     } else {
       res.status(401).json({ message: "Invalid email or password" });
