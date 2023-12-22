595ca8f10866d69b8b6b6031811e356d5d09ec6a
dzaky.fattan <70198829+DzakyFattan@users.noreply.github.com>
2023-12-22 13:56:43 +0700

    feat: text recognition

--- python/main.py
+++ python/main.py
@@ -7,9 +7,9 @@ app = FastAPI()
 import cv2
 import typing
 import numpy as np
+from itertools import groupby
 
 from mltu.inferenceModel import OnnxInferenceModel
-from mltu.utils.text_utils import ctc_decoder, get_cer
 
 class ImageToWordModel(OnnxInferenceModel):
     def __init__(self, char_list: typing.Union[str, list], *args, **kwargs):
@@ -26,6 +26,89 @@ class ImageToWordModel(OnnxInferenceModel):
         text = ctc_decoder(preds, self.char_list)[0]
 
         return text
+
+def ctc_decoder(predictions: np.ndarray, chars: typing.Union[str, list]) -> typing.List[str]:
+    """ CTC greedy decoder for predictions
+
+    Args:
+        predictions (np.ndarray): predictions from model
+        chars (typing.Union[str, list]): list of characters
+
+    Returns:
+        typing.List[str]: list of words
+    """
+    # use argmax to find the index of the highest probability
+    argmax_preds = np.argmax(predictions, axis=-1)
+
+    # use groupby to find continuous same indexes
+    grouped_preds = [[k for k,_ in groupby(preds)] for preds in argmax_preds]
+
+    # convert indexes to chars
+    texts = ["".join([chars[k] for k in group if k < len(chars)]) for group in grouped_preds]
+
+    return texts
+
+def edit_distance(prediction_tokens: typing.List[str], reference_tokens: typing.List[str]) -> int:
+    """ Standard dynamic programming algorithm to compute the Levenshtein Edit Distance Algorithm
+
+    Args:
+        prediction_tokens: A tokenized predicted sentence
+        reference_tokens: A tokenized reference sentence
+    Returns:
+        Edit distance between the predicted sentence and the reference sentence
+    """
+    # Initialize a matrix to store the edit distances
+    dp = [[0] * (len(reference_tokens) + 1) for _ in range(len(prediction_tokens) + 1)]
+
+    # Fill the first row and column with the number of insertions needed
+    for i in range(len(prediction_tokens) + 1):
+        dp[i][0] = i
+
+    for j in range(len(reference_tokens) + 1):
+        dp[0][j] = j
+
+    # Iterate through the prediction and reference tokens
+    for i, p_tok in enumerate(prediction_tokens):
+        for j, r_tok in enumerate(reference_tokens):
+            # If the tokens are the same, the edit distance is the same as the previous entry
+            if p_tok == r_tok:
+                dp[i+1][j+1] = dp[i][j]
+            # If the tokens are different, the edit distance is the minimum of the previous entries plus 1
+            else:
+                dp[i+1][j+1] = min(dp[i][j+1], dp[i+1][j], dp[i][j]) + 1
+
+    # Return the final entry in the matrix as the edit distance
+    return dp[-1][-1]
+
+def get_cer(
+    preds: typing.Union[str, typing.List[str]],
+    target: typing.Union[str, typing.List[str]],
+    ) -> float:
+    """ Update the cer score with the current set of references and predictions.
+
+    Args:
+        preds (typing.Union[str, typing.List[str]]): list of predicted sentences
+        target (typing.Union[str, typing.List[str]]): list of target words
+
+    Returns:
+        Character error rate score
+    """
+    if isinstance(preds, str):
+        preds = [preds]
+    if isinstance(target, str):
+        target = [target]
+
+    total, errors = 0, 0
+    for pred_tokens, tgt_tokens in zip(preds, target):
+        errors += edit_distance(list(pred_tokens), list(tgt_tokens))
+        total += len(tgt_tokens)
+
+    if total == 0:
+        return 0.0
+
+    cer = errors / total
+
+    return cer
 
 GCS_BUCKET_NAME = 'edims-item'
 MODEL_FILENAME = 'models/model.onnx'
@@ -64,7 +147,24 @@ def predict(input_data: str):
 
         # Perform prediction
         # Modify this part based on your specific model input processing
-        prediction = model.predict([input_data])
+        accum_cer = []
+        image_path, label = train_dataset[3]
+        image = cv2.imread(image_path)
+        model = ImageToWordModel(model_path="model.onnx", char_list=configs.vocab)
+
+        prediction_text = model.predict(image)
+
+        cer = get_cer(prediction_text, label)
+        print(f"Image: {image_path}, Label: {label}, Prediction: {prediction_text}, CER: {cer}")
+
+        # resize image by 3 times for visualization
+        image = cv2.resize(image, (image.shape[1] * 3, image.shape[0] * 3))
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+        accum_cer.append(cer)
+
+        print(f"Average CER: {np.average(accum_cer)}") 
 
         # You can return the prediction or process it further
         return {"prediction": prediction.tolist()}
