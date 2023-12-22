34ac24e810757edc2071efeb14630fccded999c9
Jeremy <73146752+JeremyRio@users.noreply.github.com>
2023-12-22 11:52:35 +0700

    feat: add deletion item
    Co-Authored-By: Dzaky Fattan <70198829+DzakyFattan@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -28,6 +28,16 @@ app.post("/register", async (req: Request, res: Response) => {
   try {
     const { email, username, password }: User = req.body;
 
+    const existingUser = await prisma.user.findFirst({
+      where: {
+        email: email,
+      },
+    });
+
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
     const newUser = await prisma.user.create({
@@ -38,11 +48,14 @@ app.post("/register", async (req: Request, res: Response) => {
       },
     });
     const { password: _, ...userWithoutPassword } = newUser;
-    res
-      .status(201)
-      .json({ message: "Register success", user: userWithoutPassword });
+    const token = generateToken(userWithoutPassword);
+    res.status(201).json({
+      message: "Register successful",
+      user: userWithoutPassword,
+      token: token,
+    });
   } catch (error) {
-    res.status(400).json({ message: "Unable to register user" });
+    res.status(500).json({ message: "Internal server error" });
   }
 });
 
@@ -140,6 +153,49 @@ app.post(
     }
   }
 );
+
+app.delete("/item/:id", verifyToken, async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const user = (req as any).user;
+    // Convert the id to a number and validate
+    const itemId = parseInt(id);
+    if (isNaN(itemId)) {
+      return res.status(400).json({ message: "Invalid item ID" });
+    }
+
+    const item = await prisma.item.findUnique({
+      where: { id: itemId },
+    });
+
+    // Check if item exist
+
+    if (!item) {
+      return res.status(404).json({ message: "Item not found" });
+    }
+
+    // Check if the item is user's
+    if (item.userid !== user.user.id) {
+      return res.status(401).json({ message: "Unauthorized deletion" });
+    }
+
+    // Delete the item from the database
+    const deletedItem = await prisma.item.delete({
+      where: { id: itemId },
+    });
+
+    // If the item is successfully deleted
+    if (deletedItem) {
+      res.status(200).json({ message: "Item deleted successfully" });
+    } else {
+      res.status(404).json({ message: "Item not found" });
+    }
+  } catch (error) {
+    // Handle any errors that occur during deletion
+    console.error(error);
+    res.status(500).json({ message: "Error deleting item" });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
